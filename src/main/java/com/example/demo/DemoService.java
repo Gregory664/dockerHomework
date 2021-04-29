f3a17b2479148c6639def3a0138d9f0549d2f437
Gregory Belevskiy <gregory664@yandex.ru>
2021-04-29 19:13:08 +0300

    get name from property file

--- src/main/java/com/example/demo/DemoService.java
+++ src/main/java/com/example/demo/DemoService.java
@@ -1,12 +1,16 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoService {
+    @Value("${name.from.file}")
+    private String nameFormFile;
+
     @GetMapping(name = "/getHello")
     public String getHello() {
-        return "Hello";
+        return String.format("Hello, %s", nameFormFile);
     }
 }
